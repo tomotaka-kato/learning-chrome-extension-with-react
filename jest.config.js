5e2e1ab7197891de7503332e1f6c57d564fa5cb3
tomo <tomotaka.k612@gmail.com>
2023-09-06 22:45:34 +0900

    update

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,14 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: [
+    "<rootDir>/tests"
+  ],
+  testMatch: [
+    "unit/**/?(*.)+(spec|test).(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  }
 };
