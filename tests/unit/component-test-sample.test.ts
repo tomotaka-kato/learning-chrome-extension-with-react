02f65bfc8020c96b4c5af8f1eb91c30491f6fb3f
tomo <tomotaka.k612@gmail.com>
2023-09-11 22:21:38 +0900

    update

--- tests/unit/component-test-sample.test.ts
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react'
-import App from '../../src/App'
-
-it('sample', () => {
-  const { getByText } = render(App())
-  expect(getByText('Vite + React')).toBeTruthy()
-})
--- tests/unit/sample.test.ts
+++
@@ -1,5 +0,0 @@
-describe('sample', () => {
-  it('sample', () => {
-    expect(1).toBe(1)
-  })
-})
